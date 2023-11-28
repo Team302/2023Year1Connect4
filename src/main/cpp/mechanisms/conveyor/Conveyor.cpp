d1884bf5b704143ece6cdb4572e92b9ce557ffa6
Frank Xia <Frank0624789@gmail.com>
2023-11-28 09:16:06 +0800

    Finish updating conveyor.cpp

--- src/main/cpp/mechanisms/conveyor/Conveyor.cpp
+++ src/main/cpp/mechanisms/conveyor/Conveyor.cpp
@@ -7,3 +7,7 @@ std::string ControlFileConveyor,
 std::string NetworkFileConveyor,
 std::shared_ptr<IDragonMotorController> motorController) : Mech1IndMotor(MechanismTypes::MECHANISM_TYPE::CONVEYOR_MECHANISM, ControlFileConveyor, NetworkFileConveyor, motorController)
 {}
+
+void Conveyor::Update(){
+    Mech1IndMotor::Update();
+}
